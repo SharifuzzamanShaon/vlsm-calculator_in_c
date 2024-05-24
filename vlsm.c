ad5faa2593e234a35c0b8cbeaf1c722595fae056
Sharifuzzaman Hasan <93692705+SharifuzzamanShaon@users.noreply.github.com>
2024-05-24 07:36:29 +0600

    Update vlsm.c

--- vlsm.c
+++ vlsm.c
@@ -11,15 +11,9 @@ int main()
     printf("Enter IP address (e.g., 192.168.1.10): ");
     scanf("%15s", ip_address_str);
     unsigned char ip_addr[4];  // Assuming IPv4 address
-    // Variables to store binary representation
-    int binary_rep[32]; // Assuming IPv4 address
-
-    // Parse the IP address string and convert each octet to binary
     sscanf(ip_address_str, "%hhu.%hhu.%hhu.%hhu", &ip_addr[0], &ip_addr[1], &ip_addr[2], &ip_addr[3]);
 
-
     printf("\n");
-
     // find the subnet mask
     int subnet_mask_decimal=0;
     int subnet_mask[4] = {0}; // Array to store the four octets of the subnet mask
@@ -84,47 +78,56 @@ int main()
 
     // calculate the subnet
     //128 64 32 16 8 4 2 1
+     int bits=0;
      int totalHostComplete, remainSubnet=totalSubent , totalSubnetDone=0;
     for (int i = 0; i < totalSubent; i++)
     {
         printf("Network %d: Total hosts = %d\n", i + 1, totalHost[i]);
         int hosts = totalHost[i]+2;
-        int bits;
+
         int sub_ip_addr;
         int totalHostInThisNet1;
         if(hosts==4)
         {
+            bits=2;
             sub_ip_addr = 4;
+             printf("subent mask /%d \n",32-2);
         }
         else if(hosts>4 && hosts<=8 )
         {
             bits=3;
             sub_ip_addr= pow(2,3);
+             printf("subent mask /%d \n",32-3);
         }
         else if(hosts>8 && hosts<=16)
         {
             bits =4;
             sub_ip_addr= pow(2,4);
+            printf("subent mask /%d \n",32-4);
         }
         else if(hosts>16 && hosts<=32)
         {
             bits =5;
             sub_ip_addr=pow(2,5);
+             printf("subent mask /%d \n",32-5);
         }
         else if(hosts>32 && hosts<=64)
         {
             bits =6;
             sub_ip_addr= pow(2,6);
+             printf("subent mask /%d \n",32-6);
         }
         else if(hosts>64 && hosts<=128)
         {
             bits =7;
             sub_ip_addr= pow(2,7);
+             printf("subent mask /%d \n",32-7);
         }
         else if(hosts>128 && hosts<=256)
         {
             bits =8;
             sub_ip_addr= pow(2,8);
+             printf("subent mask /%d \n",32-8);
         }
         int end_next_sub_ip_addr, start_sub_ip_addr=0;
 
@@ -145,31 +148,22 @@ int main()
             end_next_sub_ip_addr=0;
         }
 
+
+
         printf("N/A %d.%d.%d.%d\n", ip_addr[0], ip_addr[1], ip_addr[2], start_sub_ip_addr);
-        //printf("Subnet mask %d.%d.%d.%d\n", subnet_mask[0], subnet_mask[1], subnet_mask[2], sub_ip_addr)
-        ;
         printf("Valid range: %d.%d.%d.%d", ip_addr[0], ip_addr[1], ip_addr[2], start_sub_ip_addr+1);
         printf(" - %d.%d.%d.%d\n", ip_addr[0], ip_addr[1], ip_addr[2],end_next_sub_ip_addr + sub_ip_addr-2);
-
         printf("B/A %d.%d.%d.%d\n", ip_addr[0], ip_addr[1], ip_addr[2], end_next_sub_ip_addr + sub_ip_addr-1);
         end_next_sub_ip_addr = end_next_sub_ip_addr + sub_ip_addr;
-
         totalHostComplete = totalHostComplete + sub_ip_addr;
-        printf("totalHostComplete-%d\n",totalHostComplete);
-
         remainSubnet --;
-        //totalSubnetDone=totalSubnetDone+1;
-        printf("remainSubnet %d\n",remainSubnet);
-
         printf("\n\n");
         if(totalHostComplete >= 256 && totalHostComplete <=256 && remainSubnet> 0)
         {
             ip_addr[3]=0;
             calculateNextNetwork(ip_addr, totalHost, totalHostComplete, remainSubnet, totalSubent, num_of_host_need);
-            printf("KIhB");
             break;
         }
-
     }
 
     return 0;
@@ -179,7 +173,6 @@ int main()
 void calculateNextNetwork(unsigned char  ip_addr[],int totalHost[], int totalHostComplete, int remainSubnet, int totalSubent,  int num_of_host_need)
 {
     for (int i =totalSubent-remainSubnet; i < totalSubent; i++)
-
     {
         printf("Network %d: Total hosts = %d\n", i + 1, totalHost[i]);
         int hosts = totalHost[i]+2;
@@ -253,12 +246,10 @@ void calculateNextNetwork(unsigned char  ip_addr[],int totalHost[], int totalHos
         printf("B/A %d.%d.%d.%d\n", ip_addr[0], ip_addr[1], currNetwork, end_sub_ip_addr + sub_ip_addr-1);
         end_sub_ip_addr = end_sub_ip_addr + sub_ip_addr;
 
-
         totalHostComplete += ((end_sub_ip_addr + sub_ip_addr-1) - start_sub_ip_addr)+1;
-        printf("%d =", totalHostComplete);
+
 
         totalHostInThisNet = totalHostInThisNet + sub_ip_addr;
-        printf("totalHostInThisNet %d", totalHostInThisNet);
         printf("\n\n");
         remainSubnet--;
         if(totalHostComplete >= 256 && totalHostComplete <=256 && remainSubnet> 0 )
